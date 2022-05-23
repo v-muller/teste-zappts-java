f890ac67314409acef9c31081ccdd561580af267
Vinicio Müller <vinicio.muller@outlook.com>
2022-05-22 22:57:13 -0300

    configure JWT route configuration

--- src/main/java/com/vmuller/github/desafiozappts/security/JWTConfiguration.java
+++ src/main/java/com/vmuller/github/desafiozappts/security/JWTConfiguration.java
@@ -44,29 +44,17 @@ public class JWTConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*http.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/login").permitAll()
-                .antMatchers(HttpMethod.POST, "/players/save").permitAll()
-                .antMatchers(HttpMethod.POST, "/players").permitAll()
-                .antMatchers(HttpMethod.DELETE, "/players/{id}").permitAll()
-                .antMatchers(HttpMethod.GET, "/cards/names").permitAll()
-                .antMatchers(HttpMethod.GET, "/players/names").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
-                .addFilter(new JWTValidationFilter(authenticationManager()))
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);*/
 
         http.headers().frameOptions().disable();
         http.csrf().disable()
 //                csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 //                .and()
                 .authorizeRequests()
+                .antMatchers( HttpMethod.GET).permitAll()
                 .antMatchers( "/players/names").hasRole("USER")
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .antMatchers(HttpMethod.POST, "/players").permitAll()
                 .antMatchers( "/swagger**").permitAll()
-                .antMatchers( "/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
